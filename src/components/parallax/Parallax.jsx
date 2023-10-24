fb278b0ed736c358cc8c534cb9f836cda4deedf5
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-10-24 11:28:42 +0100

    parallax feature added

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/prop-types */
+import { useRef } from "react";
 import "./parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 function Parallax({ type }) {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <div
       className="parallax"
+      ref={ref}
       style={{
         background:
           type === "services"
@@ -12,10 +25,20 @@ function Parallax({ type }) {
             : "linear-gradient(180deg, #111132, #505064)",
       }}
     >
-      <h1>{type === "services" ? "What I can do:" : "What I have done!"}</h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
+      <motion.h1 style={{ y: yText }}>
+        {type === "services" ? "What I can do:" : "What I have done!"}
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div
+        className="planets"
+        style={{
+          y: yBg,
+          backgroundImage: `url(${
+            type === "services" ? "./planets.png" : "/sun.png"
+          })`,
+        }}
+      ></motion.div>
+      <motion.div className="stars" style={{ x: yBg }}></motion.div>
     </div>
   );
 }
