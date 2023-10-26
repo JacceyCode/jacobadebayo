caddf06a3028c92499d8638602c0d6204001e4e7
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-10-26 20:18:17 +0100

    portfolio update part 2

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,24 +9,28 @@ const items = [
     title: " My MovieBox",
     img: "./moviebox.png",
     desc: "A product for watching trailer's and checking reviews of popular movies with a search functionality to search for any movie of choice. The TMDB API was used to fetch the movie details using the CRUD method for RESTful API.",
+    url: "https://mymoviebox-jacceycode.netlify.app/",
   },
   {
     id: 2,
     title: "The Wild-Oasis Management App",
     img: "./aosis.png",
     desc: "A desktop customer booking and management app for a resort house to track customers logdement, arrivals & departures.",
+    url: "https://the-wild-oasis-vacation-house.netlify.app/",
   },
   {
     id: 3,
     title: "Image Gallery with Drag&Drop",
     img: "./drag&drop.png",
     desc: "An image gallery with the drag&drop feature, user unthentication and search functionality which is accessible to unthenticated user's only.",
+    url: "https://myimageagallery.netlify.app/",
   },
   {
     id: 4,
     title: "React Redux",
     img: "https://images.pexels.com/photos/18661408/pexels-photo-18661408/free-photo-of-man-riding-a-bike-by-the-shore.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima doloribus odit id ipsum veritatis, ab excepturi nesciunt optio iusto numquam modi, amet praesentium tempore voluptatibus fugiat rerum ratione enim quisquam.",
+    url: "https://mymoviebox-jacceycode.netlify.app/",
   },
 ];
 
@@ -50,7 +54,7 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <a href="https://mymoviebox-jacceycode.netlify.app/" target="blank">
+            <a href={item.url} target="blank">
               Check App
             </a>
           </motion.div>
