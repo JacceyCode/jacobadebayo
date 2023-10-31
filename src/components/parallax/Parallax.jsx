b89e52c2922cd188e6b74bbc6fcc0056b8d738a7
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-10-31 14:28:02 +0100

    parallax change3

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -18,12 +18,12 @@ function Parallax({ type }) {
     <div
       className="parallax"
       ref={ref}
-      // style={{
-      //   background:
-      //     type === "services"
-      //       ? "linear-gradient(180deg, #111132, #0c0c1d)"
-      //       : "linear-gradient(180deg, #111132, #505064)",
-      // }}
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)",
+      }}
     >
       <motion.h1 style={{ y: yText }}>
         {type === "services" ? "Skills..." : "Projects..."}
